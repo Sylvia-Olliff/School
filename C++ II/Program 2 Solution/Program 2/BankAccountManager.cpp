1476a7e6b02b6327065996440ca1f5777cc3cb70
Joesph Olliff <joeolliff@gmail.com>
2014-06-05 14:28:40 -0400

    Updated Program 2
    most functions finished, program complete up to 12 month projection.

--- C++ II/Program 2 Solution/Program 2/BankAccountManager.cpp
+++ C++ II/Program 2 Solution/Program 2/BankAccountManager.cpp
@@ -10,8 +10,10 @@
 using namespace std;
 
 void welcome();
-int menuRec();
-int menuNew();
+void menuRec();
+void menuNew();
+void menuDeposit(SavingsAccount&);
+void menuWithdraw(SavingsAccount&);
 
 
 
@@ -20,11 +22,104 @@ int main()
 	double bankBalance = 0.0;
 	int count = 0;
 	int selection = 0;
+	int recSelection = 0;
 	int newCust = 0;
+	bool done = false;
+	bool recDone = false;
 
-	SavingsAccount accountOne(1000.00,0.003,"005523457","1337",2);
+	SavingsAccount accountOne(7000.00,0.0025,"005523457","1337",12);
 
-	welcome();
+	do
+	{
+		welcome();
+		menuNew();
+		cin >> selection;
+		switch (selection)
+		{
+			case 1:
+			{
+				system("cls");
+				welcome();
+
+				double startBalance = 0.0;
+				double startInterestRate = 0.0025;
+				string newAccountNumber = "005524858";
+				string newCustomerID = "2277";
+				int newFreq = 12;
+
+				cout << "Please enter your starting balance: ";
+				cin >> startBalance;
+
+				SavingsAccount accountTwo(startBalance, startInterestRate, newAccountNumber, newCustomerID, newFreq);
+
+				done = true;
+				break;
+			}
+
+			case 2:
+			{
+				do
+				{
+					system("cls");
+					welcome();
+					menuRec();
+					cin >> recSelection;
+
+					switch (recSelection)
+					{
+						case 1:
+						{
+							system("cls");
+							welcome();
+							menuDeposit(accountOne);
+							break;
+						}
+
+						case 2:
+						{
+							system("cls");
+							welcome();
+							menuWithdraw(accountOne);
+							break;
+						}
+
+						case 3:
+						{
+							system("cls");
+							welcome();
+							cout << "\n\nThank You! " << endl;
+							system("pause");
+							done = true;
+							recDone = true;
+							break;
+						}
+
+						default:
+						{
+							cout << endl << endl;
+							cout << "Please enter a valid selection..." << endl;
+							system("pause");
+							system("cls");
+							break;
+						}
+					}
+				} while (!recDone);
+				break;
+			}
+
+			default:
+			{
+				cout << endl << endl;
+				cout << "Please enter a valid selection..." << endl;
+				system("pause");
+				system("cls");
+				break;
+			}
+		}
+	} while (!done);
+
+
+
 
 
 	system("pause");
@@ -36,15 +131,49 @@ void welcome()
 	cout << "Welcome to BankPro 1.0" << endl << endl;
 }
 
-int menuRec()
+void menuRec()
 {
-	int selec;
 	cout << "Please make a selection from the following menu: " << endl;
 	cout << "1. Make a Deposit" << endl;
 	cout << "2. Make a Withdrawal" << endl;
-	cout << "3. Exit" << endl << endl;
+	cout << "3. Exit" << endl;
 	cout << "Selection: ";
-	cin >> selec;
+}
 
-	return selec;
+void menuNew()
+{
+	cout << "Are you a returning customer or a new one? " << endl;
+	cout << "1. New" << endl;
+	cout << "2. Returning" << endl;
+	cout << "Selection: ";
+}
+
+void menuDeposit(SavingsAccount& activeAcc)
+{
+	double deposit = 0.0;
+	cout << fixed << setprecision(2);
+	cout << "Your current balance: $" << activeAcc.getBalance() << endl << endl;
+	cout << "Please enter the amount you wish to deposit: $";
+	cin >> deposit;
+
+	activeAcc.deposit(deposit);
+
+	cout << endl << endl << "Your new balance: $" << activeAcc.getBalance() << endl;
+	system("pause");
+	system("cls");
+}
+
+void menuWithdraw(SavingsAccount& activeAcc)
+{
+	double withdraw = 0.0;
+	cout << fixed << setprecision(2);
+	cout << "Your current balance: $" << activeAcc.getBalance() << endl << endl;
+	cout << "Please enter the amount you wish to withdraw: $";
+	cin >> withdraw;
+
+	activeAcc.withdraw(withdraw);
+
+	cout << endl << endl << "Your new balance: $" << activeAcc.getBalance() << endl;
+	system("pause");
+	system("cls");
 }
