e25ddc6f8b774cc8e4b86f1d20a7f04ee9fe241d
Joesph Olliff <joeolliff@gmail.com>
2014-06-07 12:05:54 -0400

    Finally adding Program 1

--- C++ II/Bronson 2.4 Solution/Bronson 2.4 Project/Bronson 2.4 Project.cpp
+++ C++ II/Bronson 2.4 Solution/Bronson 2.4 Project/Bronson 2.4 Project.cpp
@@ -1,51 +1,11 @@
 //FILE : Bronson 2.4 Project.cpp
-//PROG : Dave Jones (with help of Bronson's text)
-//PURP : This is the Bronson textbook Program 2.4 (pp 109 - 110)
-//Note:  this program has the class defined in the same file as main
-//In your programs the class will usually be defined in a separate header file
+//PROG : Joesph Olliff
+//PURP : To define, calculate, and describe the geometric aspects of a room.
 
 #include <iostream>
+#include "RoomType.h"
 using namespace std;
 
-class RoomType
-{
-  //Data declaration section  
-  private: 
-    double length;  //Declare length as a double variable
-    double width;   //Declare width as a double variable 
-
-  //Methods declaration section  
-  public:
-    RoomType(); //The constructor's declaration statement
-    void showRoomValues();
-    void setNewRoomValues();
-    void calculateRoomArea();
-};
-
-//Methods implementation section
-RoomType::RoomType()  //This is a constructor
-{
-  length = 25.0;
-  width = 12.0;
-  cout << "Created a new room object using the default constructor.\n\n";
-} 
-
-void RoomType::showRoomValues()   //This is an accessor
-{
-  cout << "  length = " << length 
-       << "\n  width = " << width << endl;
-}
-
-void RoomType::setNewRoomValues()   //This is a mutator 
-{  
-  length = 12.5;
-  width = 9.0;
-}
-
-void RoomType::calculateRoomArea()  //This performs a calculation
-{    
-  cout << length * width;
-}
 
 int main()
 {
@@ -55,14 +15,23 @@ int main()
   roomOne.showRoomValues();       //Use a class method on this object
   cout << "\nThe floor area of this room is: ";
   roomOne.calculateRoomArea();    //Use another class method on this object
+  cout << "\nThe Volume of the room is: ";
+  roomOne.displayVolumeOfRoom();
  
+  cout << endl << endl;
+
+  system("pause");
+  system("cls");
   roomOne.setNewRoomValues();   //Call the mutator
   
-  cout << "\n\nThe values for this room have been changed to:\n";
+  cout << "The values for this room have been changed to:\n";
   roomOne.showRoomValues();
   cout << "\nThe floor area of this room is: ";
   roomOne.calculateRoomArea();
+  cout << "\nThe Volume of the room is: ";
+  roomOne.displayVolumeOfRoom();
 
-  cout << endl;
+  cout << endl << endl;
+  system("pause");
   return 0;
 }
---
+++ C++ II/Bronson 2.4 Solution/Bronson 2.4 Project/RoomType.h
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class RoomType
+{
+	//Data declaration section  
+private:
+	double length;  //Declare length as a double variable
+	double width;   //Declare width as a double variable 
+	int height;
+
+	//Methods declaration section  
+public:
+	RoomType(); //The constructor's declaration statement
+	void showRoomValues();
+	void setNewRoomValues();
+	void calculateRoomArea();
+	void displayVolumeOfRoom();
+};
+
+//Methods implementation section
+RoomType::RoomType()  //This is a constructor
+{
+	length = 25.0;
+	width = 12.0;
+	height = 9;
+	cout << "Created a new room object using the default constructor.\n\n"; // PROFESSOR: Does this really count as the "default" constructor if we are defining what it does here?
+}
+
+void RoomType::showRoomValues()   //This is an accessor
+{
+	cout << "  length = " << length
+		<< "\n  width = " << width << endl;
+}
+
+void RoomType::setNewRoomValues()   //This is a mutator 
+{
+	length = 12.5;
+	width = 9.0;
+}
+
+void RoomType::calculateRoomArea()  //This performs a calculation
+{
+	cout << length * width;
+}
+
+void RoomType::displayVolumeOfRoom()
+{
+	cout << length * width * height;
+}
+
